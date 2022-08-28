950d9460dd0011ab8127f660b88858c892faa561
Anchal <102379804+anchal1206@users.noreply.github.com>
2022-08-28 22:12:03 +0530

    Numbe of days change for life

--- challenges of fstring.py
+++ challenges of fstring.py
@@ -1,7 +1,7 @@
 #if you live for 90 years and now youre age is 12 how many days and years and month youre left with
 age=input("what is youre current age?")
 age_as_int= int(age)
-remaning_lives=90-age_as_int
+remaning_lives=50-age_as_int
 no_days=remaning_lives*365
 no_weeks=remaning_lives*52
 no_months=remaning_lives*12
